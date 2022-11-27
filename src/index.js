c1e0d8d2151d32399ce07664ec50e23c0aef4420
Mhamad6000 <mhamadothman6000@gmail.com>
2022-11-27 13:16:47 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const description_input = document.getElementById("new-task-description");
+  const form = document.getElementById("create-task-form");
+  const task_list = document.getElementById("tasks");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const list_item = document.createElement("li");
+    list_item.innerText = description_input.value;
+    const remove_btn = document.createElement("button");
+    remove_btn.innerText = "remove";
+    remove_btn.addEventListener("click", (event) => {
+      event.target.parentNode.remove(list_item);
+    });
+    list_item.appendChild(remove_btn);
+    task_list.appendChild(list_item);
+  });
 });
